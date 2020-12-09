fed4e2959ee831c4a67d025d8621da5c14e3d082
nehahirve <neha.hirve@hyperisland.se>
2020-12-10 00:18:15 +0100

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,31 @@
-import React from "react"
+import React from 'react'
+import Menu from '../components/menu_closed.js'
+import Logo from '../components/logo.js'
+
+const colors = { pink: '#f38cdf', green: '#98c690' }
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <nav>
+        <Menu colour={colors.green} />
+      </nav>
+      <section id="splash">
+        <a href="#offers" className="logo_splash">
+          <Logo colour={colors.green} />
+        </a>
+      </section>
+      <section id="offers"></section>
+      <section className="bookings"></section>
+      <section className="login"></section>
+      <section className="about"></section>
+      <section className="contact">
+        <a href="#splash" className="logo_contact">
+          <Logo colour={colors.pink} />
+        </a>
+        <a href="">info@faraway.com</a>
+        <p>070 123 1234</p>
+      </section>
+    </>
+  )
 }
